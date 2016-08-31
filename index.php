55f6b3393a7247c7605285b717f2f15fe19dc421
Melania Miranda <melania.smc@hotmail.com>
2016-08-31 13:55:40 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,200 @@
+	<?php include('head.php'); ?>
+
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 header-section">
+
+	<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 logo">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 inner-logo">
+			<img class="img-logo" src="img/logo-club-profesional.svg" alt="" />
+		</div>
+	</div>
+
+
+
+
+
+		<div class="col-lg-9 col-md-9 col-sm-9 col-xs-12 redes-sociales">
+			<a href="#"><img src="img/facebook.svg" alt="" /></a>
+		 <a href="#"><img src="img/linkedin.svg" alt="" /></a>
+		 <a href="#"><img src="img/youtube.svg" alt="" /></a>
+
+		</div>
+
+
+		<nav class="col-lg-9 col-md-9 col-sm-9 col-xs-12 menu">
+
+			<ul>
+				<li class="menu-li">
+						<a href="#">
+							<p>
+							 Servicios <br> diferenciales
+							</p>
+						</a>
+				</li>
+
+				<li class="menu-li">
+						<a href="#">
+							<p>
+								Espacios <br> exclusivos
+							</p>
+						</a>
+				</li>
+
+				<li class="menu-li">
+						<a href="#">
+							<p>
+								Soluciones <br> financieras
+							</p>
+						</a>
+				</li>
+
+				<li class="menu-li">
+						<a href="#">
+							<p>
+								Unidades <br> de negocio
+							</p>
+						</a>
+				</li>
+
+			</ul>
+
+		</nav>
+
+
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="background-color: #BA1011; height:20px;">
+
+		</div>
+
+</div>
+
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12  video-section">
+	<div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+		<iframe width="100%" height="400" src="https://www.youtube.com/embed/hP1lpMKTTdQ?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen>
+		</iframe>
+	</div>
+	<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 form-section">
+		<div class="form-up">
+
+				<p class="p1-form">
+					GESTIONÁ <br>	 TU PASAPORTE
+				</p>
+
+				<p class="p2-form">
+					LA FORMA MÁS RÁPIDA DE CONSEGUIR BENEFICIOS
+				</p>
+
+			</div>
+
+		<div class="form-down">
+			BLALBLABLALBBLAB BLA BLA BLA BLA BLA LA BLA BLA LAB A BLA BLA LBV
+		</div>
+	</div>
+
+
+
+</div>
+
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12  card-section">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 inner-card-section">
+
+			<div class="col-lg-3 col-md-3 col-sm-3 col-xs-10 card-logo">
+
+
+				<img src="img/pasaporte-profesional.png" alt="" />
+
+			</div>
+			<div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 triangle">
+				<div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 card-logo-1">
+
+				</div>
+
+			</div>
+
+			<div class="col-lg-2 col-md-2 col-sm-2 col-xs-6 card ">
+				<p>
+					OFERTAS <br> EXCLUSIVAS
+				</p>
+				<img src="img/ofertas.svg" alt="" />
+			</div>
+
+			<div class="col-lg-2 col-md-2 col-sm-2 col-xs-6 card">
+				<p >
+					NUEVO <br> PASAPORTE
+				</p>
+				<img src="img/nuevo-pasaporte.svg" alt="" />
+			</div>
+
+			<div class="col-lg-2 col-md-2 col-sm-2 col-xs-6 card">
+				<p>
+					RED <br> M&K
+				</p>
+				<img src="img/red-mk.svg" alt="" />
+			</div>
+
+			<div class="col-lg-2 col-md-2 col-sm-2 col-xs-6 card ">
+				<p>
+					NUESTRAS <br> TIENDAS
+				</p>
+				<img src="img/nuestras-tiendas.svg" alt="" />
+			</div>
+
+		</div>
+
+</div>
+
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 promo-section">
+<p  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	CON SOLO REGISTRATE YA TENES UN VOUCHER
+</p>
+
+<div class="col-lg-11 col-md-11 col-sm-11 col-xs-12 inner-promo-section">
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 voucher">
+		<img src="img/voucher.svg" alt="" />
+	</div>
+
+	<p  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		PARA UTILIZAR EN LA PRÓXIMA COMPRA
+	</p>
+</div>
+
+
+</div>
+
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 buttons-section">
+
+	<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 button">
+		<img src="img/botones-01.svg" alt="" />
+	</div>
+
+	<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 button">
+		<img src="img/botones-02.svg" alt="" />
+	</div>
+
+	<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 button">
+		<img src="img/botones-03.svg" alt="" />
+	</div>
+
+	<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 button">
+		<img src="img/botones-04.svg" alt="" />
+	</div>
+
+	<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 button">
+		<img src="img/botones-05.svg" alt="" />
+	</div>
+
+	<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 button">
+		<img src="img/botones-06.svg" alt="" />
+	</div>
+
+	<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 button">
+		<img src="img/botones-07.svg" alt="" />
+	</div>
+
+	<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 button">
+		<img src="img/botones-08.svg" alt="" />
+	</div>
+
+
+</div>
+
+
+		<?php include('footer.php'); ?>
