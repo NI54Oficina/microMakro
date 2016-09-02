b9bd6a0fb165b8bac9a7a0e9bcd592c1a4c78f7a
Melania Miranda <melania.smc@hotmail.com>
2016-09-02 17:58:28 -0300

    17:58 2-9-2016

--- servicios-diferenciales.php
+++ servicios-diferenciales.php
@@ -7,11 +7,11 @@
 
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12  title-serv">
 
-  <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
+  <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 ">
     <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 card-logo-1">  </div>
   </div>
 
-  <div class="col-lg-11 col-md-11 col-sm-11 col-xs-11">
+  <div class="col-lg-11 col-md-11 col-sm-11 col-xs-11 center-to-parent">
     <p>
       SERVICIOS DIFERENCIALES
     </p>
