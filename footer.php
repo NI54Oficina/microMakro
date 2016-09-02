2c20954669ae47d7d6fc653d69c41d314da8b074
Melania Miranda <melania.smc@hotmail.com>
2016-09-02 17:43:44 -0300

    17:43 2-09-2016

--- footer.php
+++ footer.php
@@ -6,27 +6,39 @@
   </div>
   <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 center-to-parent">
     <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+      <a href="servicios-diferenciales.php">
         <p>
-          	 Servicios <br> diferenciales
+             Servicios <br> diferenciales
         </p>
+      </a>
+
     </div>
 
     <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+      <a href="espacios-exclusivos.php">
         <p>
         Espacios <br> exclusivos
         </p>
+      </a>
+
     </div>
 
     <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+      <a href="soluciones-financieras.php">
         <p>
         	Soluciones <br> financieras
         </p>
+      </a>
+
     </div>
 
     <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+      <a href="unidades-de-negocio.php">
         <p>
         Unidades <br> de negocio
         </p>
+      </a>
+
     </div>
 
   </div>
