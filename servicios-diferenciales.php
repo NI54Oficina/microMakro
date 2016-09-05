606d3cb97c5e43cf12dca93a3a777e04733e36d8
Melania Miranda <melania.smc@hotmail.com>
2016-09-05 11:14:22 -0300

    11:14

--- servicios-diferenciales.php
+++ servicios-diferenciales.php
@@ -7,25 +7,20 @@ $(".menu ul li:nth-child(1)").addClass("currentPage");
 </script>
   <!-- Caja de titulo -->
 
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12  title-serv">
+  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12  title-serv">
 
-  <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 ">
-    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 card-logo-1">  </div>
-  </div>
-<<<<<<< HEAD
+    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
+      <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 card-logo-1">  </div>
+    </div>
+    <div class="hidden-sm hidden-xs">
+  	<div style="width:10px;background-color:white;position:absolute;height:79px;"></div>
 
-  <div class="col-lg-11 col-md-11 col-sm-11 col-xs-11 center-to-parent">
-=======
-	<div class="hidden-sm hidden-xs">
-	<div style="width:10px;background-color:white;position:absolute;height:79px;"></div>
-	
-	<div class="triangle" style="position:absolute;padding-top:0 !important;width:30px;padding-left:10px;margin-top:20px;">
-          <div class="col-lg-10 col-md-12 col-sm-12 col-xs-12 card-logo-1">  </div>
-        </div>
-	</div>
-  <div class="col-lg-11 col-md-11 col-sm-11 col-xs-11">
-	
->>>>>>> origin/master
+  	<div class="triangle" style="position:absolute;padding-top:0 !important;width:30px;padding-left:10px;margin-top:20px;">
+            <div class="col-lg-10 col-md-12 col-sm-12 col-xs-12 card-logo-1">  </div>
+          </div>
+  	</div>
+    <div class="col-lg-11 col-md-11 col-sm-11 col-xs-11">
+
     <p>
       SERVICIOS DIFERENCIALES
     </p>
